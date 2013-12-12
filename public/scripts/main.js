02ecdcaa472448b008fb973cdc73898b61c91668
ponyspy <desade4me@gmail.com>
2013-12-12 14:48:33 +0400

    new scroll

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,25 +1,21 @@
-var count = 0;
-var margin = 250;
-
-function getScroll () {
-	var offset_top = $('.cont').eq(count).offset().top;
-	var offset_bottom = $('.cont').eq(count).offset().top + $('.cont').eq(count).height();
-	var scroll_top = $(document).scrollTop();
-
-	if (scroll_top >= offset_bottom) {
-		count++;
-		margin = 250;
-	}
-
-	if (scroll_top >= offset_top + 100) {
-		if (margin < 550) {
-			margin+=5.33333333333333333;
-			$('.cont').eq(count+1).css('margin-top', margin)
-		}
-	}
-}
-
-
 $(document).ready(function() {
-	$(document).on('scroll', getScroll)
+
+	var scrollorama = $.scrollorama({
+		blocks:'.scrollblock',
+		enablePin:false
+	});
+
+	// scrollorama.onBlockChange(function() {
+	// 	var i = scrollorama.blockIndex;
+	// 	$('#console')
+	// 		.css('display','block')
+	// 		.text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
+	// });
+
+	scrollorama.animate('#one',{ duration:600, property:'margin-top', start:350, end:0})
+						 .animate('#two',{ duration:600, delay: -50, property:'margin-top', start:150, end:650})
+						 		.animate('#inner1',{ duration:600, delay: 250, property:'margin-left', start:150, end:0})
+						 		.animate('#inner2',{ duration:600, delay: 250, property:'margin-left', start:0, end:150})
+						 .animate('#three',{ duration:2000, delay:600, property:'margin-top', start:100, end: 500})
+						 .animate('#four',{ duration:2000, delay:2300, property:'margin-top', start:100, end: 500});
 });
