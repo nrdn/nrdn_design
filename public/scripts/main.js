e6719d263c40411d9888b958344e45e552f9db2f
ponyspy <desade4me@gmail.com>
2013-12-12 18:00:04 +0400

    works proto

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,11 +11,14 @@ $(document).ready(function() {
 	// 		.css('display','block')
 	// 		.text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
 	// });
+	//
+	//
+	//
 
 	scrollorama.animate('#one',{ duration:600, property:'margin-top', start:350, end:0})
-						 .animate('#two',{ duration:600, delay: -50, property:'margin-top', start:150, end:650})
-						 		.animate('#inner1',{ duration:600, delay: 250, property:'margin-left', start:150, end:0})
-						 		.animate('#inner2',{ duration:600, delay: 250, property:'margin-left', start:0, end:150})
+						 .animate('#works',{ duration:600, delay: -50, property:'margin-top', start:150, end:650})
+						 		.animate('.even',{ duration:600, delay: 250, property:'margin-left', start:180, end:0})
+						 		.animate('.odd',{ duration:600, delay: 250, property:'margin-left', start:0, end:180})
 						 .animate('#three',{ duration:2000, delay:600, property:'margin-top', start:100, end: 500})
 						 .animate('#four',{ duration:2000, delay:2300, property:'margin-top', start:100, end: 500});
 });
