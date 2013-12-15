8f9f02ce89a69e3f9b3bfdc0d8254a6435d3fad1
ponyspy <desade4me@gmail.com>
2013-12-15 17:19:22 +0400

    pin menu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 	var scrollorama = $.scrollorama({
-		blocks:'.scrollblock',
-		enablePin:false
+		blocks:'.scrollblock'
 	});
 
 	// scrollorama.onBlockChange(function() {
@@ -17,7 +16,9 @@ $(document).ready(function() {
 
 	scrollorama.animate('#menu',{ duration:600, delay:-50, property:'margin-top', start:450, end:10})
 						 .animate('.m_item:odd',{ duration:300, delay:-20, property:'margin-top', start:250, end:0})
-						 // .animate('.m_item:even',{ duration:300, delay:-20, property:'margin-top', start:0, end:250})
+
+
+	scrollorama.animate('#one',{ duration:600, delay:600, property:'margin-top', start:350, end:0, pin:true})
 
 
 	scrollorama.animate('#works',{ duration:600, delay: 550, property:'margin-top', start:150, end:650})
@@ -25,7 +26,6 @@ $(document).ready(function() {
 						 .animate('.w_block:even',{ duration:600, delay: 550, property:'margin-left', start:0, end:180})
 
 
-	scrollorama.animate('#one',{ duration:600, delay:600, property:'margin-top', start:350, end:0})
-						 .animate('#three',{ duration:2000, delay:600, property:'margin-top', start:100, end: 500})
+	scrollorama.animate('#three',{ duration:2000, delay:600, property:'margin-top', start:100, end: 500})
 						 .animate('#four',{ duration:2000, delay:2300, property:'margin-top', start:100, end: 500});
 });
