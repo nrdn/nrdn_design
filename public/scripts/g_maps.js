36ff0d2f0d9ea37ea89ca26fab0a84f74facbd39
ponyspy <desade4me@gmail.com>
2014-03-13 18:46:06 +0400

    map

--- public/scripts/g_maps.js
+++ public/scripts/g_maps.js
@@ -1,27 +1,39 @@
 var map;
+var grayStyles = [
+	{
+		"stylers": [
+			{ "saturation": -100 },
+			{ "gamma": 0.5 },
+			{ "invert_lightness": true },
+			{ "lightness": -27 }
+		]
+	}
+];
+
 function initialize() {
 	var myLatlng = new google.maps.LatLng(55.7638563, 37.6131137);
 
-  var mapOptions = {
-    zoom: 18,
-    center: myLatlng
-  };
+	var mapOptions = {
+		zoom: 18,
+		styles: grayStyles,
+		center: myLatlng
+	};
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
 
 	var infowindow = new google.maps.InfoWindow({
-	    content: 'Народный архитектор'
+			content: 'Народный архитектор'
 	});
 
 	var marker = new google.maps.Marker({
-	    position: myLatlng,
-	    map: map,
-	    title: 'Народный архитектор'
+			position: myLatlng,
+			map: map,
+			title: 'Народный архитектор'
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
-	    infowindow.open(map, marker);
+			infowindow.open(map, marker);
 	});
 }
 
