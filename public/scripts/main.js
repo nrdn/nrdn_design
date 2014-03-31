801082dba140673a72027b57812ae92681078de1
ponyspy <desade4me@gmail.com>
2014-03-31 18:18:54 +0400

    animate

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-	var h = 100;
-	$(document).scroll(function(event) {
-		if (h >= 50) {
-			h-=2;
-			$('.inner').css({'width': h + 'px', 'height': h + 'px'});
-		}
-	});
+var s = skrollr.init();
 });
