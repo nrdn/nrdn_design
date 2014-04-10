257d28b6d21165ad98a6625f163480bde2c289f4
ponyspy <desade4me@gmail.com>
2014-04-10 13:54:05 +0400

    new maket

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-var s = skrollr.init();
+	var s = skrollr.init({
+		constants: {
+			works: document.getElementById("works").clientHeight + 800
+		},
+		forceHeight: false
+	});
+
+	skrollr.menu.init(s, {
+	    //skrollr will smoothly animate to the new position using `animateTo`.
+	    animate: true,
+
+	    //Multiply your data-[offset] values so they match those set in skrollr.init
+	    scale: 2,
+
+	    //How long the animation should take in ms.
+	    duration: function(currentTop, targetTop) {
+	    	return 500;
+	    }
+	});
 });
