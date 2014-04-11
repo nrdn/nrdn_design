77f98a80f1e528ed30030b98709a793e09274fe9
ponyspy <desade4me@gmail.com>
2014-04-11 16:20:24 +0400

    maket broken

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	$('#work_area, .m_images').hide();
+
+	$('.work').click(function(event) {
+		$('#work_area, .m_images').show();
+		$('#works, #categorys, #contacts, #maps').hide();
+		$('.maket').css('margin-bottom', '10%');
+	});
+
+	$('#work_area').click(function(event) {
+		$('#work_area, .m_images').hide();
+		$('#works, #categorys, #contacts, #maps').show();
+		$('.maket').removeAttr('style');
+	});
+
 	var s = skrollr.init({
 		constants: {
 			works: document.getElementById("works").clientHeight + 800
