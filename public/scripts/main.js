6c2f4ca2f139d3f07efad104f73e6a01c2963725
ponyspy <desade4me@gmail.com>
2014-04-14 16:41:26 +0400

    works

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
 
 	$('.work_item').click(function(event) {
+		var arr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
+
 		$.post( '/work', { id: $(this).attr('id') } ).done(function(work) {
 			$(document).scrollTop(0)
 			$('.main_block').css('margin-bottom', '0px').css('margin', '0 auto');
 			$('#works, #categorys, #contacts, #maps').hide();
 
 			$.each(work.images, function(index, image) {
-				var img_link = $('<a/>', {'class': 'm_img_item', 'href': '#cool'});
+				var img_link = $('<a/>', {'class': 'm_img_item', 'href': '#' + arr[index]});
 				var img = $('<img/>', {'src': image});
-				var demo = $('<img/>', {'src': image});
+				var demo = $('<img/>', {'src': image, id: arr[index]});
 
 				$('#work_area').append(demo);
 				$('.m_images').append(img_link.append(img));
 			});
-			$('#work_area, .m_images').show();
+			$('#work_area, .m_images').fadeIn(500);
 		});
 	});
 
 	$('#m_works, #m_categorys, #m_contacts').click(function(event) {
-		$('#work_area, .m_images').hide();
-		$('.m_images, #work_area').empty();
+		$('#work_area, .m_images').fadeOut(500).promise().done(function() {
+			$('.m_images, #work_area').empty();
+		});
 		$('#works, #categorys, #contacts, #maps').show();
 		$('.main_block').removeAttr('style');
 	});
